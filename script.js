b75765fdf78f9652197cd9aa1f0a108141c586c8
Xiaoxu (Carter) Wu <carter.wu@ucla.edu>
2016-10-23 00:32:18 -0700

    com

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var map;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
         center: { lat: -33.865427, lng: 151.196123 },
-        mapTypeId: 'terrain'
+        mapTypeId: google.maps.MapTypeId.HYBRID
     });
 
     $.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function (data, status) {
