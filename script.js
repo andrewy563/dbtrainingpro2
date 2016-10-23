682baa8fb4f0e5c1162b932a4af8f7c045af3d9d
Xiaoxu (Carter) Wu <carter.wu@ucla.edu>
2016-10-23 00:13:58 -0700

    added map

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-$(document).ready(function() {
-
-    $(".refresh").click(function(){
-        $.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson", function(data, status){
-            console.log(data);
-        });
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 2,
+        center: { lat: -33.865427, lng: 151.196123 },
+        mapTypeId: 'terrain'
     });
-});
+
+    $.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson", function (data, status) {
+        console.log(data);
+        function eqfeed_callback(results) {
+            var heatmapData = [];
+            for (var i = 0; i < results.features.length; i++) {
+                var coords = results.features[i].geometry.coordinates;
+                var latLng = new google.maps.LatLng(coords[1], coords[0]);
+                heatmapData.push(latLng);
+            }
+            var heatmap = new google.maps.visualization.HeatmapLayer({
+                data: heatmapData,
+                dissipating: false,
+                map: map
+            });
+        }
+        eqfeed_callback(data);
+    });
+}
+
+
